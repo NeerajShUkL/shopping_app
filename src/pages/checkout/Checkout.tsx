d596f5807c18954b596ca2d4f2af194bb1ec3f4b
Neeraj Shukla <neerajshukla9102@gmail.com>
2023-01-11 19:17:46 +0530

    creating checkout form

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -1,12 +1,65 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
-const Checkout: React.FC = () => {
-  return (
-      <Box sx={{ flexGrow: 1, m: "auto", marginTop: 10, minHeight: "70%", p: 3 }}>
-        this is checkout
-      </Box>
-  )
+interface ShippingAddres {
+  fname: string;
+  lname: string;
+  address: string;
 }
 
-export default Checkout
+const Checkout: React.FC = () => {
+  const [shippingA, setShippingA] = useState<ShippingAddres>({
+    fname: "",
+    lname: "",
+    address: "",
+  });
+
+  const handleShippingAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setShippingA({ ...shippingA, [name]: value });
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        display: "flex",
+        flexGrow: 1,
+        m: "auto",
+        marginTop: 10,
+        minHeight: "70%",
+        p: 3,
+      }}
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="First Name"
+          value={shippingA.fname}
+          name="fname"
+          onChange={handleShippingAddress}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Last Name"
+          value={shippingA.lname}
+          name="lname"
+          onChange={handleShippingAddress}
+        />
+        <TextField
+          fullWidth
+          required
+          id="outlined-required"
+          label="Address"
+          value={shippingA.address}
+          name="address"
+          onChange={handleShippingAddress}
+        />
+      </div>
+    </Box>
+  );
+};
+
+export default Checkout;
